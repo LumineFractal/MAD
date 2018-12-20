038148fda3c9edd079fb724f9c72e69087627643
mateusz <mazsak97@gmail.com>
2018-12-20 14:52:07 +0100

    Create package

---
+++ MP_MAD/MP_MAD-core/src/main/java/facade/Facade.java
@@ -0,0 +1,38 @@
+package facade;
+
+import player.Player;
+import player.PlaylistManager;
+import sources.Track;
+
+import java.util.List;
+
+public class Facade {
+    private PlaylistManager playlistManager;
+    private Player player;
+
+    public void playTrack(Track track){
+
+    }
+
+    public void setVolume(double volume){
+
+    }
+
+    public void createPlaylist(String name){
+
+    }
+
+    public void removePlaylist(String name){
+
+    }
+
+    public void addtrack(int idx, Track track){
+
+    }
+
+    public void editPlaylist(int idx, List<Track> tracks){
+
+    }
+
+    
+}
